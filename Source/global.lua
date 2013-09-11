ed0539c3413200c6c965d942110a4522c00d4364
DataDink <markerdink@gmail.com>
2013-09-11 11:07:26 -0700

    in progress

--- Source/global.lua
+++ Source/global.lua
@@ -4,6 +4,7 @@ end
 
 turtlecraft = {};
 turtlecraft.directory = "turtlecraft_data/";
+
 turtlecraft.math = {};
 turtlecraft.math.round = function(number)
 	if (number % 1 < 0.5) then
--- Source/position.lua
+++ Source/position.lua
@@ -53,17 +53,23 @@ turtlecraft.position = {};
 			return intended; 
 		end
 		
+		reader = string.gmatch(line, "[^,]+");
+		local previous = {
+			x = tonumber(reader()),
+			y = tonumber(reader()),
+			z = tonumber(reader()),
+			d = tonumber(reader())
+		};
+		
 		if (fuel > turtle.getFuelLevel()) then
 			intended.positionConfirmed = true;
+		elseif (fuel == turtle.getFuelLevel()) then
+			intended.x = previous.x;
+			intended.y = previous.y;
+			intended.z = previous.z;
+			intended.positionConfirmed = true;
 		end
-		
-		reader = string.gmatch(line, "[^,]+");
-		if (not intended.positionConfirmed)
-			intended.x = tonumber(reader());
-			intended.y = tonumber(reader());
-			intended.z = tonumber(reader());
-			intended.d = tonumber(reader());
-		end
+
 		return intended;
 	end
 	cache.write = function(intended, previous) 
@@ -84,190 +90,90 @@ turtlecraft.position = {};
 		d = directions.forward,
 		positionConfirmed = false,
 		directionConfirmed = false,
-		hasCustom = false,
-		hasGps = false,
-		hasCompass = false
+		canSync = false,
 	};
 	
 	-- Most reliable - but, sadly,  you may not be playing on RenEvo's custom server.
-	location.tryGetCustom = function() 
-		-- TODO: add code here for RenEvo's custom location mod
-		return nil;
+	location.tryUpdateCustom = function()
+		-- Unknown yet
 	end
+	location.tryUpdateCustom();
 	
-	-- Second most reliable - requires wonky GPS setup. - no facing support
-	location.tryGetGps = function() 
+	location.tryReadGps()
 		if (rednet == nil or gps == nil) then return nil; end
 		rednet.open("right");
 		if (not rednet.isOpen("right")) then return nil; end
 		local x, z, y = gps.locate(10); -- I orientate my coordinates differently
-		if (x == nil) then return nil; end
-		return {
-			x = x,
-			y = y,
-			z = z
-		};
+		return x, y, z;
 	end
 	
-	-- Gives us a reliable facing. - no position support
-	location.tryGetCompass = function() 
-		if (getFacing == nil) then return nil; end
-		return facings[getFacing()];
+	-- Second most reliable - requires wonky GPS setup. - no facing support
+	location.tryUpdateGps = function()
+		local x, y, z = location.tryReadGps();
+		if (x == nil) then return; end
+		location.x = x;
+		location.y = y;
+		location.z = z;
+		location.positionConfirmed = true;
+		location.canSync = true;
 	end
+	location.tryUpdateGps();
 	
-	location.init = function() 
-		local customReading = location.tryGetCustom();
-		if (customReading ~= nil) then location.hasCustom = true; end
-		local gpsReading = location.tryGetGps();
-		if (gpsReading ~= nil) then location.hasGps = true; end
-		local compassReading = location.tryGetCompass();
-		if (compassReading ~= nil) then location.hasCompass = true; end
-		local cacheReading = cache.read();
+	location.tryUpdateCompass = function()
+		if (getFacing == nil) then return; end
+		location.d = facings[getFacing()];
+		location.directionConfirmed = true;
+	end
+	location.tryUpdateCompass();
+	
+	-- if true than an adjustment was made and should be compensated by the caller
+	location.trySync = function()
+		if (location.directionConfirmed) then return false; end
+		if (not location.canSync) then return false; end
 		
-		location.x = cacheReading.x;
-		location.y = cacheReading.y;
-		location.z = cacheReading.z;
-		location.d = cacheReading.d;
-		location.positionConfirmed = cacheReading.positionConfirmed;
-		location.directionConfirmed = cacheReading.directionConfirmed;
+		local x, y, z = location.tryReadGps();
+		local actual = location.d;
+		if (location.d == directions.forward and x < location.x) then actual = directions.left; end
+		if (location.d == directions.backward and x < location.x) then actual = directions.left; end
+		if (location.d == directions.right and x < location.x and y == location.y) then actual = directions.left; end
+		if (location.d == directions.forward and x > location.x) then actual = directions.right; end
+		if (location.d == directions.backward and x > location.x) then actual = directions.right; end
+		if (location.d == directions.left and x > location.x and y == location.y) then actual = directions.right; end
+		if (location.d == directions.forward and y < location.y and x == location.x) then actual = directions.backward; end
+		if (location.d == directions.left and y < location.y) then actual = directions.backward; end
+		if (location.d == directions.right and y < location.y) then actual = directions.backward; end
+		if (location.d == directions.backward and y > location.y and x == location.x) then actual = directions.forward; end
+		if (location.d == directions.left and y > location.y) then actual = directions.forward; end
+		if (location.d == directions.right and y > location.y) then actual = directions.forward; end
 		
-		if (location.hasCustom) then
-			location.x = customReading.x;
-			location.y = customReading.y;
-			location.z = customReading.z;
-			location.d = customReading.d;
-			location.positionConfirmed = true;
-			location.directionConfirmed = true;
-		elseif (location.hasGps) then
-			location.x = gpsReading.x;
-			location.y = gpsReading.y;
-			location.z = gpsReading.z;
-			location.positionConfirmed = true;
+		local adjustRequired = x ~= location.x or y ~= location.y or z ~= location.z or actual ~= location.d;
+		if (adjustRequired) then
+			location.x = x;
+			location.y = y;
+			location.z = z;
+			location.d = actual;
 		end
-		if (location.hasCompass) then
-			location.d = compassReading;
-			location.directionConfirmed = true;
-		end
+		return adjustRequired;
 	end
-	location.init();
 	
-	location.confirm = function(beforeMove) -- assumes a forward movement only
-		if (location.hasGps and (not location.directionConfirmed)) then
-			local actualDirection = beforeMove.d
-			if (beforeMove.x > location.x) then actualDirection = directions.right; end
-			if (beforeMove.x < location.x) then actualDirection = directions.left; end
-			if (beforeMove.y > location.y) then actualDirection = directions.forward; end
-			if (beforeMove.y < location.y) then actualDirection = directions.backward; end
-			if (actualDirection ~= beforeMove.d) then
-				if (not turtle.back()) then return false; end
-				location.d = actualDirection;
-				location.directionConfirmed = true;
-				location.face(beforeMove.d);
-				if (not turtle.forward()) then return false; end
-			end
+	turtlecraft.position.get = function() {
+		return location.x, location.y, location.z, location.d;
+	}
+	
+	-- Will fail if a syncronization change happened.
+	turtlecraft.position.set = function(x, y, z, d, moveAction) {
+		local previous = {x = location.x, y = location.y, z = location.z, d = location.d};
+		local intended = {x = x, y = y, z = z, d = d};
+		if (moveAction ~= nil) then
+			cache.write(indended, previous);
+			moveAction();
+			if (location.trySync()) then return false; end
 		end
+		cache.write(intended);
+		location.x = x;
+		location.y = y;
+		location.z = z;
+		location.d = d;
 		return true;
-	end
-	
-	location.face = function(direction)
-		if (direction == location.d) then return; end
-		if (direction == directions.up or direction == directions.down) then return; end
-		if (direction % 90 ~= 0) then error("Position.lua location.face: direction was not % 90 degrees"); end
-		if (direction == (location.d + 270) % 360) then
-			turtle.turnLeft();
-			location.d = direction;
-			return;
-		end
-		while (direction ~= location.d) do
-			turtle.turnRight();
-			location.d = location.d + 90;
-		end
-	end
-	
-	location.move = function(direction, before, after, fail)
-		local priorPosition = {x = location.x, y = location.y, z = location.z, d = location.d};
-		location.face(direction);
-		local expectedPosition = {x = location.x, y = location.y, z = location.z, d = location.d};
-		
-		local move = turtle.forward;
-		if (direction == directions.up) then 
-			move = turtle.up; 
-			expectedPosition.z = expectedPosition.z + 1;
-		end
-		if (direction == directions.down) then 
-			move = turtle.down; 
-			expectedPosition.z = expectedPosition.z - 1;
-		end
-		if (direction == directions.forward) then expectedPosition.x = expectedPosition.x + 1; end
-		if (direction == directions.backward) then expectedPosition.x = expectedPosition.x - 1; end
-		if (direction == directions.left) then expectedPosition.y = expectedPosition.y - 1; end
-		if (direction == directions.right) then expectedPosition.y = expectedPosition.y + 1; end
-		
-		if (before ~= nil) then before(); end
-		
-		cache.write(expectedPosition, priorPosition);
-		local moveTries = 0;
-		while (not move()) do
-			moveTries = moveTries + 1;
-			if (moveTries > 10) then 
-				cache.write(location);
-				return false; 
-			end
-			sleep(1);
-			if (fail ~= nil) then fail(); end
-		end
-		cache.write(location);
-		if (not location.confirm(priorPosition)) then return false; end
-		location.x = expectedPosition.x;
-		location.y = expectedPosition.y;
-		location.z = expectedPosition.z;
-		if (after ~= nil) then after(); end
-		return true;
-	end
-	
-	location.moveTo = function(targetPosition, before, after, fail)
-		local x = turtlecraft.math.round(targetPosition.x);
-		local y = turtlecraft.math.round(targetPosition.y);
-		local z = turtlecraft.math.round(targetPosition.z);
-		local xmove = nil;
-		if (location.x > x) then xmove = directions.left; end
-		if (location.x < x) then xmove = directions.right; end
-		local ymove = nil;
-		if (location.y > y) then ymove = directions.backward; end
-		if (location.y < y) then ymove = directions.forward; end
-		local zmove = nil;
-		if (location.z > z) then zmove = directions.down; end
-		if (location.z < z) then zmove = directions.up; end
-		
-		while (location.z ~= z) do
-			if (not location.move(zmove, before, after, fail)) then return false; end
-		end
-		while (location.x ~= x) do
-			if (not location.move(xmove, before, after, fail)) then return false; end
-		end
-		while (location.y ~= y) do
-			if (not location.move(ymove, before, after, fail)) then return false; end
-		end
-		return true;
-	end
-	
-	turtlecraft.position.getPosition = function() 
-		return {x = location.x, y = location.y, z = location.z, d = location.d};
-	end
-	
-	turtlecraft.position.inSync = function()
-		return location.positionConfirmed and location.position.directionConfirmed;
-	end
-	
-	turtlecraft.position.canSync = function()
-		return (location.positionConfirmed or location.hasCustom or location.hasGps) and (location.directionConfirmed or location.hasCustom or location.hasCompass);
-	end
-	
-	turtlecraft.position.face = function(direction)
-		cache.write({x = location.x, y = location.y, z = location.z, d = direction}, location);
-		location.face(direction);
-		cache.write(location);
-	end
-	
-	turtlecraft.position
+	}
 end)();

89b0bc1918255b222e58470701c1d615a01f83d2
DataDink <markerdink@gmail.com>
2013-09-19 21:53:11 -0700

    In progress

--- Source/excavate.lua
+++ Source/excavate.lua
@@ -141,8 +141,6 @@ turtlecraft.excavate = {};
 		plot.reset();
 	end
 	move.next = function()
-		print(plot.home.d);
-		print(plot.home.x);
 		local resumeDist = plot.calcDistance(plot.progress.x, plot.progress.y, plot.progress.z);
 		local homeDist = plot.calcReturn();
 		local fuel = turtlecraft.fuel.estimateRemaining();

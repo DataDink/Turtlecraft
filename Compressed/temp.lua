6f4ba9f3b6046196667c6336585f9abc4674fbd9
DataDink <markerdink@gmail.com>
2013-09-21 15:30:04 -0700

    In progress

--- Compressed/temp.lua
+++ Compressed/temp.lua
@@ -1,6 +1,4 @@
 local manifest = http.get("https://raw.github.com/DataDink/Turtlecraft/master/Source/manifest");
-print(manifest);
-print(manifest.readLine);
 local fileName = manifest.readLine();
 while (fileName ~= nil) do
 	print(fileName);

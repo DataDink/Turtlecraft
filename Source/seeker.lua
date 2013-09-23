9336bdb2377eca019c0cac52d80a2bf6957520d9
DataDink <markerdink@gmail.com>
2013-09-22 19:26:18 -0700

    In progress

--- Source/seeker.lua
+++ Source/seeker.lua
@@ -5,44 +5,7 @@ turtlecraft.seeker = {};
 	local path = turtlecraft.directory .. "seeker.data";
 	local turtleBack = function(action) turtle.turnRight(); turtle.turnRight(); local result = action(); turtle.turnRight(); turtle.turnRight(); return result; end
 	local directions = { up = "up", down = "down", forward = "forward" };
-	local methods = {
-		up = {
-			detect = turtle.detectUp,
-			detectBack = turtle.detectDown,
-			move = turtle.up,
-			moveBack = turtle.down,
-			dig = turtle.digUp,
-			digBack = turtle.digDown,
-			compare = turtle.compareUp,
-			compareBack = turtle.compareDown,
-			place = turtle.placeUp,
-			placeBack = turtle.placeDown
-		},
-		down = {
-			detect = turtle.detectDown,
-			detectBack = turtle.detectUp,
-			move = turtle.down,
-			moveBack = turtle.up,
-			dig = turtle.digDown,
-			digBack = turtle.digUp,
-			compare = turtle.compareDown,
-			compareBack = turtle.compareUp,
-			place = turtle.placeDown,
-			placeBack = turtle.placeUp
-		},
-		forward = {
-			detect = turtle.detect,
-			detectBack = function() return turtleBack(turtle.detect); end,
-			move = turtle.forward,
-			moveBack = turtle.back,
-			dig = turtle.dig,
-			digBack = function() return turtleBack(turtle.dig); end,
-			compare = turtle.compare,
-			compareBack = function() return turtleBack(turtle.compare); end,
-			place = turtle.place,
-			placeBack = function() return turtleBack(turtle.place); end
-		}
-	};
+	local pattern = {turtle.turnRight, turtle.turnLeft, turtle.turnLeft, turtle.turnLeft};
 	
 	local cache = {};
 	cache.write = function(func, direction)
@@ -70,61 +33,56 @@ turtlecraft.seeker = {};
 					return i; 
 				end
 			end
+			turtlecraft.term.clear("Fill");
 			turtlecraft.term.write(1, 5, "Please add more inventory...");
 			turtlecraft.input.onInventory();
 			turtlecraft.term.write(1, 5, "Resuming in 15 seconds...");
 			sleep(15);
-			turtlecraft.term.write(1, 5, "Resuming");
+			turtlecraft.term.write(1, 5, "Press Q to stop");
 		end
 	end
-	
-	local seekPattern = function(direction, empty)
-		if (direction == directions.up and turtle.detectDown() ~= empty) then return methods.down; end
-		if (direction == directions.down and turtle.detectUp() ~= empty) then return methods.up; end
 		
-		local pattern = {turtle.turnRight, turtle.turnLeft, turtle.turnLeft, turtle.turnLeft};
-		for i, turn in pairs(pattern) do
-			turn();
-			if (turtle.detect() ~= empty) then return methods.forward; end
-		end
-		
-		if (turtle.detectDown() ~= empty) then return methods.down; end
-		if (turtle.detectUp() ~= empty) then return methods.up; end
-		turtle.turnRight();
-		turtle.turnRight();
-		return nil;
-	end
-	
-	local seekAndRecover = function(direction, empty)
-		local result = seekPattern(direction, empty);
-		if (result ~= nil) then return result; end
-		turtle.up();
-		for v = 1, 3 do 
-			for h = 1, 4 do
-				turtlecraft.fuel.require(3);
-				turtle.forward();
-				turtle.forward();
-				turtle.turnRight();
-				result = seekPattern;
-				if (result ~= nil) then return result; end
-			end
-		end
-		return nil;
-	end
-	
-	-- the oddball
 	turtlecraft.seeker.fill = function(direction)
 		if (direction == nil) then 
 			direction = directions.down; 
 			if (turtle.detectDown() and not turtle.detectUp()) then direction = directions.up; end
 		end
 		cache.write("fill", direction);
+		local priority = { move = turtle.moveUp, detect = turtle.detectUp };
+		local progress = { move = turtle.moveDown, detect = turtle.detectDown, place = turtle.placeDown };
+		if (direction == directions.up) then
+			priority = { move = turtle.moveDown, detect = turtle.detectDown };
+			progress = { move = turtle.moveUp, detect = turtle.detectUp, place = turtle.placeUp };
+		end
 		
-		turtlecraft.term.clear();
+		local step = function()
+			for i, turn in pairs(pattern) do
+				if (turtle.back()) then return true; end
+			end
+			return false;
+		end
+		
+		turtlecraft.term.clear("Fill");
 		turtlecraft.term.write(1, 5, "Press Q to stop");
 		turtlecraft.input.escapeOnKey(16, function() 
 			while true do
-				turtle.turnRight();
+				turtlecraft.fuel.require(1);
+				if (priority.detect()) then 
+					priority.move();
+				elseif (step()) then
+					selectSlot();
+					turtle.place();
+				else
+					turtle.turnLeft();
+					turtle.turnLeft();
+					if (progress.detect()) then
+						turtlecraft.term.write(1, 5, "I got stuck!");
+						turtlecraft.term.write(1, 6, "Press any key to continue");
+						turtlecraft.input.readKey();
+						return;
+					end
+					progress.move();
+				end							
 			end
 		end);
 	end

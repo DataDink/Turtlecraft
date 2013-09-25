fd1d3a7d3025def2d32d614e83422327ca8eccab
DataDink <markerdink@gmail.com>
2013-09-25 06:39:35 -0700

    Debugging

--- Source/debug.lua
+++ Source/debug.lua
@@ -1,7 +1,7 @@
 local files = http.get("https://raw.github.com/DataDink/Turtlecraft/master/Source/manifest");
 for file in files.readLine do
 	print('file');
-	local exec, err = loadstring(http.get("https://raw.github.com/DataDink/Turtlecraft/master/Source/" .. file));
+	local exec, err = loadstring(http.get("https://raw.github.com/DataDink/Turtlecraft/master/Source/" .. file).readAll());
 	if (err ~= nil) then error(err); end
 	exec();
 end

1a690a930ac94f68e80ef2d876df149ce3a42bd8
DataDink <markerdink@gmail.com>
2015-06-25 20:15:34 -0700

    ...

--- 2.0/input.lua
+++ 2.0/input.lua
@@ -1,8 +1,16 @@
 function turtlecraft:input()
+   local shift = false;
+   local caps = false;
+   local ctrl = false;
+   local alt = false;
 
    function self.readKey()
       local _, key = os.pullEvent('key');
       return key;
+   end
+
+   function self.readChar()
+      
    end
 
    function self.readText(filter)
@@ -18,11 +26,38 @@ function turtlecraft:input()
    end
 
    function self.character(code)
-      if (code == keys.escape) return false, false; end
-      if (code >= keys.a and code <= keys.z) return keys.getName(code), key.getName(code).upper(); end
-      if (code >= keys.one and code <= keys.nine) return tostring(code - keys.one + 1); end
-      if (code >= keys.numPadOne and code <= keys.numPadNine) return tostring(code - keys.numPadOne + 1); end
-      if (code == keys.zero or code == keys.numPadZero) return '0'; end
-      if (code == keys.minus or code == keys.numPadSubtract) return '-'; end
+      for i, v in pairs(codes) do
+         if (v == code)
    end
+
+
+   local codes = {
+      keys.a, keys.b, keys.c, keys.d, keys.e, keys.f, keys.g, keys.h, keys.i, keys.j, keys.k,
+      keys.l, keys.m, keys.n, keys.o, keys.p, keys.q, keys.r, keys.s, keys.t, keys.u, keys.v,
+      keys.w, keys.x, keys.y, keys.z,
+      keys.one, keys.two, keys.three, keys.four, keys.five, keys.six, keys.seven, keys.eight,
+      keys.nine, keys.zero,
+      keys.numPad1, keys.numPad2, keys.numPad3, keys.numPad4, keys.numPad5, keys.numPad6,
+      keys.numPad7, keys.numPad8, keys.numPad9, keys.numPad0,
+      keys.numPadSubtract, keys.numPadAdd, keys.numPadDecimal, keys.numPadEquals,
+      keys.numPadEnter, keys.numPadComma, keys.numPadDivide,
+      keys.minus, keys.equals, keys.tab, keys.leftBracket, keys.rightBracket, keys.enter,
+      keys.semiColon, keys.apostrophe, keys.grave, keys.backslash, keys.comma, keys.period,
+      keys.slash, keys.multiply, keys.space, keys.colon, keys.underscore
+   };
+   local lowercase = {
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+      's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+      '-', '+', '.', '=', '\r', ',', '/', '-', '=', ' ', '[', ']', '\r', ';', "'", '`', '\\',
+      ',', '.', '/', '*', ' ', ':', '_'
+   };
+   local uppercase = {
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'
+      'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+      '_', '+', ' ', '{', '}', '\r', ':', '"', '~', '|', '<', '>', '?', '*', ' ', ':', '_'
+   };
 end

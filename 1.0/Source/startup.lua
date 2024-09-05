a0ca98470dcde6b2109cb6a9902b8f73bb6ba2fa
Mark Greenwald <markerdink@gmail.com>
2024-09-05 16:25:57 -0700

    Delete 1.0 directory

--- 1.0/Source/startup.lua
+++
@@ -1,4 +0,0 @@
-local file = http.get("https://raw.github.com/DataDink/Turtlecraft/master/Current/turtlecraft.lua").readAll();
-local exec, err = loadstring(file);
-if (err ~= nil) then error(err); end
-exec();

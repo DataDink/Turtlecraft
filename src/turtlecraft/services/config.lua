62aab12dae00a2536991c6a818c4582636d720e8
Mark Greenwald <markerdink@gmail.com>
2024-09-05 16:25:39 -0700

    Delete src directory

--- src/turtlecraft/services/config.lua
+++
@@ -1,7 +0,0 @@
-TurtleCraft.export('services/config', function()
-  -- NOTE: cfgjson will be added to the turtlecraft scope at build time
-  local config =  TurtleCraft.import('services/json').parse(cfgjson or '{}');
-  config.recoveryPath = config.recoveryPath:gsub('[%s/]+$', '');
-  config.logsPath = config.logsPath:gsub('[%s/]+$', '');
-  return config;
-end);
--- src/turtlecraft/ui/input.lua
+++
@@ -1,9 +0,0 @@
-TurtleCraft.export('ui/user-input', function()
-  local view = TurtleCraft.import('ui/views/input');
-  return {
-    show = function(text)
-      view.show(text);
-      return read();
-    end
-  }
-end);

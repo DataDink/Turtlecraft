92ecd4afaddd68a53f71a80cacaa4f6f2fe0e5f3
Mark Greenwald <markerdink@gmail.com>
2024-09-25 16:19:27 -0700

    Update recover.lua

--- recover.lua
+++ recover.lua
@@ -1,22 +1,20 @@
-if (#arg < 1) then
-  print('Recover will configure this computer to run a command each time a computer boots.')
-  print('Example: recover.lua myprogram.lua arg1 arg2')
-  shell.exit()
+print('Recover will configure this computer to run a command each time a computer boots.')
+print('Example: recover.lua myprogram.lua arg1 arg2')
+
+if (#arg > 0) then
+  os.setComputerLabel(arg[1])
+  
+  local startup = 'shell.run('
+  for i,v in ipairs(arg) do
+    startup = startup .. "'" .. v .. "'"
+    if (i < #arg) then startup = startup .. ', ' end
+  end
+  startup = startup .. ')\n'
+  
+  local file = io.open('startup.lua', 'w+')
+  file:write(startup)
+  file:flush()
+  file:close()
+  
+  os.reboot()
 end
-
-os.setComputerLabel(arg[1])
-
-local startup = 'shell.run('
-for i,v in ipairs(arg) do
-  startup = startup .. "'" .. v .. "'"
-  if (i < #arg) then startup = startup .. ', ' end
-end
-startup = startup .. ')\n'
-
-local file = io.open('startup.lua', 'w+')
-file:write(startup)
-file:flush()
-file:close()
-
-os.reboot()
-

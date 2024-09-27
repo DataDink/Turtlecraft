9e2d7c2e2bfd2bd0a08a7d85eb360bc22e4fe7d4
Mark Greenwald <markerdink@gmail.com>
2024-09-27 08:18:40 -0700

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,15 +1,19 @@
 local source = 'https://api.github.com/repos/DataDink/TurtleCraft'
+local whitelist = {
+  '.lua',
+  '.api'
+}
+
+print('')
 print('************************')
 print('Downloading from: ' .. source)
-print()
+print('')
 
 local directory = textutils.unserializeJSON(http.get(source .. '/contents').readAll())
 if (directory) then
   for i,v in ipairs(directory) do
-    local extension = v.name:sub(-4);
-    local filter = extension == '.lua'
-                or extension == '.api'
-    if (filter) then
+    local extension = v.name:sub(v.name:find('.[^.]+$'));
+    if (whitelist[extension]) then
       print('* ' .. v.name)
       local content = http.get(v.download_url).readAll()
       local file = io.open(v.name, 'w+')

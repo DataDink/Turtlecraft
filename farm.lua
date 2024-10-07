e9e34fbd9a5e30b9bd6a97dbba59b2172f4d7a72
Mark Greenwald <markerdink@gmail.com>
2024-10-07 11:55:08 -0700

    Create farm.lua

---
+++ farm.lua
@@ -0,0 +1,19 @@
+os.loadAPI('turtle.crop.api')
+
+local timer = args and args[1] or 10
+local direction = args and args[2] == "down" or false
+
+while (true) do
+  if (turtle.crop.mature()) then turtle.dig() end
+  if (not turtle.detect()) then
+    for (i = 1,16) do
+      if (turtle.getItemCount(i) > 0 and turtle.select(i) and turtle.place()) then break end
+    end
+  end
+  for (i = 1,16) do
+    if (turtle.getItemCount(i) > 0 and turtle.select(i)) then 
+      if (direction) then turtle.dropDown() else turtle.dropUp() end
+    end
+  end
+  os.sleep(timer)
+end

08bd254a30a4671ccb4f010785bfa4f45981b5fc
Mark Greenwald <markerdink@gmail.com>
2024-10-07 12:01:49 -0700

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -6,11 +6,11 @@ local direction = args and args[2] == "down" or false
 while (true) do
   if (turtle.crop.mature()) then turtle.dig() end
   if (not turtle.detect()) then
-    for (i = 1,16) do
+    for i = 1,16 do
       if (turtle.getItemCount(i) > 0 and turtle.select(i) and turtle.place()) then break end
     end
   end
-  for (i = 1,16) do
+  for i = 1,16 do
     if (turtle.getItemCount(i) > 0 and turtle.select(i)) then 
       if (direction) then turtle.dropDown() else turtle.dropUp() end
     end

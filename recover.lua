e64be33782805b003e6ec2b1718f08cfff718761
Mark Greenwald <markerdink@gmail.com>
2024-11-06 05:28:45 -0800

    Update recover.lua

--- recover.lua
+++ recover.lua
@@ -1,20 +1,38 @@
 print('Recover will configure this computer to run a command each time a computer boots.')
 print('Example: recover.lua myprogram.lua arg1 arg2')
 
-if (#arg > 0) then
-  os.setComputerLabel(arg[1])
-  
-  local startup = 'shell.run('
-  for i,v in ipairs(arg) do
-    startup = startup .. "'" .. v .. "'"
-    if (i < #arg) then startup = startup .. ', ' end
-  end
-  startup = startup .. ')\n'
-  
-  local file = io.open('startup.lua', 'w+')
-  file:write(startup)
-  file:flush()
-  file:close()
-  
-  os.reboot()
+if (not arg or #arg == 0) then
+  print()
+  print('no program specified')
+  return
 end
+
+local files = fs.complete(arg[1])
+if (not files or #files == 0) then
+  print()
+  print(arg[1] .. ' not found')
+  return
+end
+
+if (#files > 1) then
+  print()
+  print(arg[1] .. ' ambiguous: ' .. table.concat(files, ','))
+  return
+end
+
+local file = files[1]
+os.setComputerLabel('Job: ' .. string.gsub(file, '%.%w+', ''))
+
+local startup = 'shell.run(' .. table.concat(
+for i = 2,#arg do
+  startup = startup .. "'" .. arg[i] .. "'"
+  if (i < #arg) then startup = startup .. ', ' end
+end
+startup = startup .. ')\n'
+
+local file = io.open('startup.lua', 'w+')
+file:write(startup)
+file:flush()
+file:close()
+
+os.reboot()

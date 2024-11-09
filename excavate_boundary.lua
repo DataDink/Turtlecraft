992b0de48ba7069ce8f8176a30195507367f23c7
Mark Greenwald <markerdink@gmail.com>
2024-11-09 14:37:06 -0800

    Update excavate_boundary.lua

--- excavate_boundary.lua
+++ excavate_boundary.lua
@@ -2,6 +2,7 @@ if (not turtle) then error("excavate_boundary requires a turtle") end
 
 os.loadAPI('turtle.track.api')
 os.loadAPI('turtle.boundary.api')
+turtle.select(1)
 
 local resuming = arg and arg[1] == "resume"
 if (not resuming) then
@@ -12,13 +13,53 @@ end
 function display(message)
   term.clear()
   term.setCursorPos(1,1)
-  print("Excavates a rectangular boundary down to bedrock.")
+  print("Excavates a rectangular boundary.")
   print("Attempts to recover when reloaded.")
   print("Requires fuel in slot #1")
   print("")
   print(message)
 end
+message("")
 
-function checkFuel()
-  
+function refuel(req)
+  if (turtle.getFuelLevel() > req) then return true end
+  while (turtle.getFuelLevel() < req) do
+    if (turtle.getItemCount(1) < 2 or not turtle.refuel(1)) do
+      message("Refueling: need more fuel in slot 1")
+      os.sleep(1)
+    end
+  end
+end
+
+function surface()
+  if (turtle.track.get() == 0) then return true end
+  local move = turtle.track.get() > 0 and turtle.track.down or turtle.track.up end
+  while (turtle.track.get() ~= 0) do
+    refuel(1)
+    move()
+  end  
+end
+
+function full()
+  for i = 2,16 do
+    if (turtle.getItemCount(i) == 0) then return false end
+  end
+  return true
+end
+
+function dig()
+  while (not turtle.detectDown() or turtle.digDown() or not turtle.detectDown() or turtle.attackDown()) do
+    turtle.track.down()
+  end
+  surface()
+end
+
+while (true) do
+  while (full()) do
+    surface()
+    message("Inventory: awaiting unload")
+    os.sleep(1)
+  end
+  dig()
+  turtle.boundary.next()
 end

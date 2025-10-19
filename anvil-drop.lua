9e90d00359277a2f4e2a5c874e04eb97baea1151
Greenwald <Greenwald@Greenwald>
2025-10-19 13:30:29 -0700

    anvil/place experimenting

--- anvil-drop.lua
+++ anvil-drop.lua
@@ -12,73 +12,102 @@ end
 function instruct()
   term.clear()
   term.setCursorPos(1, 1)
-  print("** Anvil Drop **")
+  print("** Anvil Drop Instructions **")
   print()
-  print("- Put a block placer above the turtle")
-  print("- Insert Anvil")
-  print("- Insert Item(s)")
+  print("* Place turtle with inventory below it.")
+  print("* Place wall 1 space in front of both.")
+  print("* Place anvil and items in the inventory.")
+  print("* After completion the turtle emits redstone.")
   print()
-  print("Press enter to continue...")
+  print("Press enter to start...")
   waitEnter()
 end
 
-function find()
-  for i = 1, 16 do
-    local detail = turtle.getItemCount(i)
-    if (detail) then
-      if (detail.name == "minecraft:anvil" or detail.name == "minecraft:chipped_anvil" or detail.name == "minecraft:damaged_anvil") then
-        return i
-      end
+function complain(message)
+  term.clear()
+  term.setCursorPos(1, 1)
+  print("** Anvil Drop Fixes **")
+  print()
+  print(message)
+  print()
+  os.sleep(3)
+end
+
+function inspect()
+  if (turtle.detect()) then
+    complain("There should be 1 empty space in front.")
+    return false
+  end
+  local _, bottom = peripheral.getType("bottom")
+  if (not bottom or bottom ~= "inventory") then
+    complain("The inventory should be below the turtle.")
+    return false
+  end
+  local inventory = peripheral.wrap("bottom")
+  local items = inventory.list()
+  local hasAnvil = false
+  local hasItems = false
+  for _, item in pairs(items) do
+    if (item.name == "minecraft:anvil" or item.name == "minecraft:chipped_anvil" or item.name == "minecraft:damaged_anvil") then
+      hasAnvil = true
+    elseif (item.count > 0) then
+      hasItems = true
     end
+  end
+  if (not hasAnvil) then
+    complain("The anvil should be in the inventory.")
+    return false;
+  end
+  if (not hasItems) then
+    complain("The items should be in the inventory.")
+    return false;
+  end
+  return true
+end
+
+function suck()
+  local inventory = peripheral.wrap("bottom")
+  while (#inventory.list() > 0 and turtle.suckDown()) do end
+  if (#inventory.list() > 0) then
+    complain("Failed to load items from the inventory.")
+    return false
+  end
+  return true
+end
+
+function drop()
+  for i = 1, 16 do
+    local info = turtle.getItemDetail(i)
+    if (info and info.name ~= "minecraft:anvil" and info.name ~= "minecraft:chipped_anvil" and info.name ~= "minecraft:damaged_anvil") then
+      turtle.select(i)
+      turtle.drop()
+    end
+  end
+  for i = 1, 16 do
+    local info = turtle.getItemDetail(i)
+    if (info and (info.name == "minecraft:anvil" or info.name == "minecraft:chipped_anvil" or info.name == "minecraft:damaged_anvil")) then
+      turtle.select(i)
+      turtle.place()
+      return true
+    end
+  end
+  complain("Failed to complete the anvil drop.")
+  return false
+end
+
+function emit()
+  for _, side in pairs(redstone.getSides()) do
+    redstone.setOutput(side, true)
+  end
+  os.sleep(1)
+  for _, side in pairs(redstone.getSides()) do
+    redstone.setOutput(side, false)
   end
 end
 
-function complain(items)
-  term.clear()
-  term.setCursorPos(1, 1)
-  print("** Anvil Drop **")
-  print()
-  print("Missing anvil...")
-  print()
-  os.sleep(2)
-end
-
-function waitAnvil()
-  term.clear()
-  term.setCursorPos(1, 1)
-  print("** Anvil Drop **")
-  print()
-  print("Awaiting anvil drop...")
-  while (not turtle.detect()) do os.sleep(0.1) end
-end
-
-if (turtle.detect()) then turtle.dig() end
-turtle.suck()
-
 while (true) do
   instruct()
-
-  local found = find()
-
-  if (found) then
-    for i = 1, 16 do
-      if (i ~= found and turtle.getItemCount(i) > 0) then
-        turtle.select(i)
-        turtle.drop(1)
-      end
-    end
-    turtle.select(found)
-    turtle.dropUp()
-    redstone.setOutput("top", true)
-    os.sleep(1)
-    redstone.setOutput("top", false)
-    waitAnvil()
-
-    turtle.dig()
-    turtle.suck()
-    os.sleep(1)
-    turtle.suck()
-  else 
-    complain()
+  if (inspect() and suck() and drop()) then 
+    emit() 
   end
 end
--- place.lua
+++ place.lua
@@ -1,48 +1,75 @@
 if (not turtle) then error("Error: Drop requires a turtle") end
 
-local direction = arg and tostring(arg[1]) or 'front'
-local rstone = arg and string.lower(tostring(arg[2])) == 'true'
+local PLACE = arg and tostring(arg[1]) or 'front'
+local PULL = arg and string.lower(tostring(arg[2])) ~= 'false'
+local RSTONE = arg and string.lower(tostring(arg[3])) ~= 'false'
 
 function display(message)
   term.clear()
   term.setCursorPos(1,1)
   print("Place will place blocks either above, below or in front of the turtle.")
-  print('It will optionally wait for a redstone signal.')
-  print('place [<direction:up/down/front> [<redstone:true/false>]]')
+  print('It will optionally pull from an inventory and/or wait for a redstone signal.')
+  print('place [<place:up/down/front> [<pull:true/false> [<redstone:true/false>]]]')
   print('')
   print(message)
 end
 
-function locate()
-  for i = 1, 16 do
-    if (turtle.getItemCount(i) > 0) then return i end
+function signal()
+  for _, side in pairs(redstone.getSides()) do
+    if (redstone.getInput(side)) then
+      return true
+    end
   end
+  return false
+end
+
+function inventory()
+  for i = 1, 16 do
+    if (turtle.getItemCount(i) > 0) then
+      return i
+    end
+  end
+  if (not PULL) then return false end
+  for side in pairs({"top", "bottom", "front"}) do
+    local _, type = peripheral.getType(side)
+    if (type == "inventory") then
+      if (#peripheral.call(side, "list") > 0) then
+        return side
+      end
+    end
+  end
+  return false
 end
 
 function wait()
-  while (true) do
-    if (rstone) then
-      os.pullEvent("redstone")
-      for _, side in pairs(redstone.getSides()) do
-        if (redstone.getInput(side)) then return end
-      end
-    else
-      if (locate()) then return end
-      os.sleep(1)
-    end
+  while (RSTONE and not signal()) do pullEvent("redstone") end
+  local items = inventory()
+  while (items == false) do
+    os.sleep(1)
+    items = inventory()
   end
+  return items
 end
 
-function place()
-  local slot = locate()
-  turtle.select(slot)
-  if (direction == 'up') then return turtle.placeUp() end
-  if (direction == 'down') then return turtle.placeDown() end
+function place(from)
+  if (type(from) == "number") then
+    turtle.select(from)
+  elseif (from == "top") then
+    turtle.suckUp()
+  elseif (from == "bottom") then
+    turtle.suckDown()
+  elseif (from == "front") then
+    turtle.suck()
+  end
+  if (PLACE == "up") then return turtle.placeUp()
+  elseif (PLACE == "down") then return turtle.placeDown()
   return turtle.place()
 end
 
 while (true) do
-  display("")
-  wait()
-  place()
+  display("Waiting...")
+  local items = wait()
+  display("Placing...")
+  place(items)
+  os.sleep(0.1)
 end
